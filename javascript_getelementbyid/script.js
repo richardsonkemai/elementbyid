7773e91555becb33c380a71277ede9bc344af1ac
richardsonkemai <46830623+richardsonkemai@users.noreply.github.com>
2019-01-28 14:38:59 -0800

    Add files via upload

---
+++ javascript_getelementbyid/script.js
@@ -0,0 +1,22 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+	
+// "el" stands for the "element" that was just clicked
+function allOpaque(el){
+	document.getElementById("face").classList.add("opaque");
+	document.getElementById("noface").classList.add("opaque");	
+	document.getElementById("prayer").classList.add("opaque");
+}
+
+// "el" stands for the "element" that was just clicked
+function removeColor(el){
+	document.getElementById("pats-column").classList.remove("pats-silver");
+	document.getElementById("rams-column").classList.remove("rams-blue");
+	
+}
+
+function showTrophy(el){
+	document.getElementById("ramspic").src = "images/lombardi_trophy.png";
+	document.getElementById("patspic").src = "images/lombardi_trophy.png";
+	document.getElementById("nopic").src = "images/lombardi_trophy.png";
+}
